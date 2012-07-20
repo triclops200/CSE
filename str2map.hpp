0a819b5abf70fd982ee30af3b5d5af5c24ec61a7
MiningMarsh <joshua.songy@gmail.com>
2012-07-20 19:56:19 -0400

    Merge?

---
+++ str2map.hpp
@@ -0,0 +1,12 @@
+/*
+ * str2map.hpp
+ *
+ *  Created on: Jul 20, 2012
+ *      Author: joshua
+ */
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+map<string,string> resolveKeys(char* KeyString);
